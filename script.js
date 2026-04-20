fbd0072867c811fde62dcef89046d493a77d555b
DIVYANSHU KUMAR MAURYA <155415613+Dvyanshu1050@users.noreply.github.com>
2026-04-20 22:56:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// button select karo
+let btn = document.querySelector('input[type="button"]');
+
+btn.addEventListener("click", function () {
+
+  let select = document.getElementById("colorSelect");
+
+  // selected index lo
+  let index = select.selectedIndex;
+
+  // remove karo
+  if (index !== -1) {
+    select.remove(index);
+  }
+
+});
